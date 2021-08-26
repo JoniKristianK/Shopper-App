1ce4a863808ef53f901e81a18b14f15c2e1a4e75
JoniKristianK <joni.karilainen@gmail.com>
2021-08-26 15:04:29 +0300

    Add: Redux & ProductsOverviewScreen

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from ' react-redux'
+
+import productReducer from './store/reducers/products'
+
+const rootReducer = combineReducers({
+  products: productReducer,
+})
+
+const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>SHOPPER-APP</Text>
-    </View>
+    <Provider store={store}>
+      <View>
+        <Text>SHOPPER-APP</Text>
+      </View>
+    </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
