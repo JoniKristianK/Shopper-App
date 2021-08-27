a340986eeb91f0e388bc814c499e76e0d667d5ed
JoniKristianK <joni.karilainen@gmail.com>
2021-08-27 08:54:00 +0300

    Add: ProductsOverviewScreen navigation

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 
 import ProductItem from '../../components/shop/ProductItem'
 
-const ProductsOverviewScreen = () => {
+const ProductsOverviewScreen = ({ navigation }) => {
   const products = useSelector((state) => state.products.availableProducts)
   return (
     <FlatList
@@ -15,7 +15,12 @@ const ProductsOverviewScreen = () => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewDetail={() => {}}
+          onViewDetail={() => {
+            navigation.navigate('ProductDetail', {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title,
+            })
+          }}
           onAddToCart={() => {}}
         />
       )}
